3ced56801c3a5993ddf4bbd1cc56ee9b4830a3e8
dexterchan <dexterchan@gmail.com>
2017-01-21 21:52:47 +0800

    run service shell script

--- scripts/runService.sh
+++ scripts/runService.sh
@@ -1,1 +1,1 @@
-python ntl_OneNNclusterService.py --input ~/workspace/PyLogAnalysis/sample/ErrorSample.log.txt --output ./outputCluster.log
+python ntl_OneNNclusterService.py --input ~/workspace/PyLogAnalysis/sample/ErrorSample.log.txt --output ./outputCluster.log &
